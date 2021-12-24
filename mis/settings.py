6f85576577dd3438f94975995e024bd6a8782819
hrk2023 <hrkdasgupta025@gmail.com>
2021-12-25 02:17:21 +0530

    feat: add opportunity subscriber and publisher classes, setup admin routes and controllers

--- mis/settings.py
+++ mis/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin',
+    'company',
+    'student',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
